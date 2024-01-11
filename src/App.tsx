36556e59c846e423bebdf15ecb528432ad816180
Manik <tonysins43@gmail.com>
2024-01-12 01:16:49 +0600

    project setup with react,redux,typescript,tailwindcss and shadcnui

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="font-bold">My todo</h1>
+      <Button>My Button</Button>
+    </>
+  );
+}
+
+export default App;
