37d83629284e44c7c1460d3836e500f636a5d859
Manik <tonysins43@gmail.com>
2024-01-12 01:19:32 +0600

    setup redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {},
 });
 
-export default store;
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
